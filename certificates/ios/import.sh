297f05f25993caf3089cf6f5605de7b49b6f1d32
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2020-07-21 19:35:06 +0300

    Refactor import script

--- certificates/ios/import.sh
+++ certificates/ios/import.sh
@@ -1,14 +1,21 @@
 
 echo "Install profiles..."
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
-cp dev.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/86ce4d81-fd7e-46b2-ae6a-3092b4af6cd7.mobileprovision
+UUID=`/usr/libexec/PlistBuddy -c 'Print :UUID' /dev/stdin <<< $(security cms -D -i ./dev.mobileprovision)`
+cp dev.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/$UUID.mobileprovision
+ls ~/Library/MobileDevice/Provisioning\ Profiles
+echo "Done."
 
+KEYCHAIN=~/Library/Keychains/build.keychain
 echo "Install dev certificate..."
-security create-keychain -p "" build.keychain
-security import dev.p12 -t agg -k ~/Library/Keychains/build.keychain -P "$IOS_PASSPHRASE" -A
-
-security list-keychains -s ~/Library/Keychains/build.keychain
-security default-keychain -s ~/Library/Keychains/build.keychain
-security unlock-keychain -p "" ~/Library/Keychains/build.keychain
-
-security set-key-partition-list -S apple-tool:,apple: -s -k "" ~/Library/Keychains/build.keychain
+echo "Configure keychain"
+security create-keychain -p "" "$KEYCHAIN"
+security list-keychains -s "$KEYCHAIN"
+security default-keychain -s "$KEYCHAIN"
+security unlock-keychain -p "" "$KEYCHAIN"
+security set-keychain-settings
+security list-keychains
+echo "Import..."
+security import dev.p12 -t agg -k "$KEYCHAIN" -P "$IOS_PASSPHRASE" -A
+security set-key-partition-list -S apple-tool:,apple: -s -k "" "$KEYCHAIN"
+echo "Done."
