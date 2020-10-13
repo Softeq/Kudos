45c067354e8a92dbf43f006c65971ba8075fb344
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2020-10-13 17:53:25 +0300

    Check build

--- src/cloud_functions/functions/src/index.ts
+++ src/cloud_functions/functions/src/index.ts
@@ -5,6 +5,9 @@ import notifications = require('./services/user_notifications');
 admin.initializeApp();
 const db = admin.firestore();
 
+// test commit
+
+
 /**
  * Trigger updates information about Team in achievements collection
  * when Team name changes
