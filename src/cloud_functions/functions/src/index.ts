2e217f2e2b20cd538319132611f892cabb97d92d
vadim.pylsky <vadim.pylsky@softeq.com>
2020-11-19 16:24:05 +0300

    fix build

--- src/cloud_functions/functions/src/index.ts
+++ src/cloud_functions/functions/src/index.ts
@@ -5,7 +5,6 @@ admin.initializeApp();
 export { addUsers } from './http/addUsers'
 export { cleanupStorage } from './storage/cleanup'
 export { createAchievementReferences } from './database/achievement_references/onCreate'
-export { createUser } from './database/users/onCreate'
 export { updateAchievement } from './database/achievements/onUpdate'
 export { updateTeam } from './database/teams/onUpdate'
 export { updateUser } from './database/users/onUpdate'
